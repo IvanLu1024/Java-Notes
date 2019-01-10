1678d36771127f78f3a5d2e890a849c99ce50b32
DuHouAn <18351926682@163.com>
2019-01-10 21:57:36 +0800

    二叉树和递归

---
+++ LeetCodeSolutions/src/code_05_binaryTree/Code_863_AllNodesDistanceKInBinaryTree.java
@@ -0,0 +1,21 @@
+package code_05_binaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 863. All Nodes Distance K in Binary Tree
+ *
+ * We are given a binary tree (with root node root),
+ * a target node, and an integer value K.
+ * Return a list of the values of all nodes that have a distance K from the target node.
+ * The answer can be returned in any order.
+ */
+public class Code_863_AllNodesDistanceKInBinaryTree {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        List<Integer> res=new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+    }
+}
