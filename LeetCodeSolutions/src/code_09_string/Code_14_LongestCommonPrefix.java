3ffa935d45cf7503f6f499ba7952152b57425b21
DuHouAn <18351926682@163.com>
2019-01-14 11:09:00 +0800

    字符串

--- LeetCodeSolutions/src/code_09_string/Code_14_LongestCommonPrefix.java
+++ LeetCodeSolutions/src/code_09_string/Code_14_LongestCommonPrefix.java
@@ -2,6 +2,8 @@ package code_09_string;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * 14. Longest Common Prefix
  *
@@ -18,7 +20,12 @@ import org.junit.Test;
  Explanation: There is no common prefix among the input strings.
  */
 public class Code_14_LongestCommonPrefix {
-    public String longestCommonPrefix(String[] strs) {
+    /**
+     * 思路一：
+     * 要想获取公共前缀字符串,只需要以第一个字符串作为基准就好了
+     * 取出第一个字符串中指定位置的元素，看后续字符串是否在该指定位置存在该元素。
+     */
+    public String longestCommonPrefix1(String[] strs) {
         if(strs==null || strs.length==0){
             return "";
         }
@@ -43,9 +50,32 @@ public class Code_14_LongestCommonPrefix {
         return res.toString();
     }
 
+    /**
+     * 思路二：
+     * 先利用Arrays.sort(strs)为数组排序，再将数组第一个元素和最后一个元素的字符从前往后对比即可！
+     */
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        if (strs.length == 1) {
+            return strs[0];
+        }
+        Arrays.sort(strs);
+        StringBuilder res=new StringBuilder();
+        int minLen= Math.min(strs[0].length(),strs[strs.length-1].length());
+        for(int i = 0; i<minLen;i++){
+            if(strs[0].charAt(i)!=strs[strs.length-1].charAt(i)) {
+                break;
+            }
+            res.append(strs[0].charAt(i));
+        }
+        return res.toString();
+    }
+
     @Test
     public void test(){
-        String[] strs={"flower","flow","flight"};
+        String[] strs = {"flower","flow","flight"};
         System.out.println(longestCommonPrefix(strs));
     }
 }
